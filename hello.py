36a930af7f6c3ee7127b3b49ef8b6b1a51bf747a
Adam Zaidi <93174158+adamzaidi@users.noreply.github.com>
2024-03-14 10:10:21 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,13 @@ def hello(string):
     string = 'hello'
     print(string)
     
+<<<<<<< Updated upstream
 def sup(string):
     string = "sup"
     print("sup")
+=======
+
+def pleasework():
+    string = "pleasework"
+    print(string)
+>>>>>>> Stashed changes
